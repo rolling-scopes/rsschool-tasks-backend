fe9782fc4906912ab0539936b99335259f396964
Oleh Duleba <oleh_duleba@epam.com>
2023-12-18 12:29:59 +0200

    added conversations-clear.ts, groups-clear.ts, users-clear.ts

---
+++ src/functions/users-clear.ts
@@ -0,0 +1,42 @@
+import { DynamoDBClient, ScanCommand, DeleteItemCommand } from "@aws-sdk/client-dynamodb";
+import {APIGatewayProxyEventV2} from "aws-lambda";
+
+const client = new DynamoDBClient({ region: 'eu-central-1' });
+
+export const handler = async (event: APIGatewayProxyEventV2) => {
+
+    const getCommand = new ScanCommand({
+        TableName: 'rsschool-2023-users',
+        ExpressionAttributeNames: {
+            "#E": "email",
+        },
+        ProjectionExpression: "#E",
+    });
+    const result = await client.send(getCommand);
+
+    const deleteRequests = result.Items.map(item => {
+        const email = item.email.S;
+
+        const deleteItemCommand = new DeleteItemCommand({
+            TableName: "rsschool-2023-users",
+            Key: {
+                email: {
+                    S: email,
+                },
+            },
+            ReturnValues: "NONE",
+        });
+        const deleteItemQuery = client.send(deleteItemCommand);
+
+
+        return deleteItemQuery.then(res => ({email, result: res.status === 'fulfilled' ? res.value : res.reason}));
+    });
+
+    const response = await Promise.allSettled(deleteRequests);
+
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response),
+    };
+};
