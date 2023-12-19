4e448f88d3cd7548876668d0dd6289af6a705ee2
Oleh Duleba <oleh_duleba@epam.com>
2023-12-19 12:39:12 +0200

    added conversations-count.ts function

---
+++ src/functions/conversations-count.ts
@@ -0,0 +1,42 @@
+
+import {DynamoDBClient, ScanCommand, ListTablesCommand, ListTablesCommandInput} from "@aws-sdk/client-dynamodb";
+import {APIGatewayProxyEventV2} from "aws-lambda";
+
+const client = new DynamoDBClient({ region: 'eu-central-1' });
+
+
+export const handler = async (event: APIGatewayProxyEventV2) => {
+    const getCommand = new ScanCommand({
+        TableName: 'rsschool-2023-conversations',
+        ExpressionAttributeNames: {
+            "#ID": "id",
+        },
+        ProjectionExpression: "#ID",
+    });
+    const result = await client.send(getCommand);
+
+    let tables: string[] = [];
+    let lastTable;
+
+    do {
+        const input: ListTablesCommandInput = {
+            Limit: 100,
+        };
+
+        if (lastTable) {
+            input.ExclusiveStartTableName = lastTable;
+        }
+
+        const listCommand = new ListTablesCommand(input);
+
+        const reply = await client.send(listCommand);
+
+        tables.push(...reply.TableNames);
+
+        lastTable = reply.LastEvaluatedTableName;
+    } while (lastTable);
+
+
+
+    return {statusCode: 200, body: JSON.stringify({list: result, tables})};
+};
